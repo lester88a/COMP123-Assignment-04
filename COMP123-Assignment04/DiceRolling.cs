7afedb296c0db645bbafece1a40fcab0245dfaba
lester88a <lester88a@gmail.com>
2015-03-20 23:37:47 -0400

    Created RollDice.cs

---
+++ COMP123-Assignment04/DiceRolling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment04_Rolling_Dice
+{
+    class DiceRolling
+    {
+        //Instance variables++++++++++++++++++++++++++
+        private int[] _diceType;
+        private int[] _sum;
+        private Random _rnd;
+
+        //public properties+++++++++++++++++++++++++++
+        public int[] totalOfSum 
+        { 
+            get
+            {
+                return this._sum;
+            } 
+        
+        }
+
+        //constructor++++++++++++++++++++++++++++++++
+        public DiceRolling()
+        {
+            this._diceType = new int[2];
+            this._sum = new int[11];
+            this._rnd = new Random();
+        }
+
+        //public method+++++++++++++++++++++++++++++++
+        public void RollDice(int numberOfTimes)
+        {
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                this._diceType[0] = this._rnd.Next(1, 7);
+                this._diceType[1] = this._rnd.Next(1, 7);
+                this._sum[(this._diceType[0] + this._diceType[1]) - 2]++;
+            }
+        }
+    }
+}
